314c82c573ef6cda031cce7daf85a0fff245e1f1
Abdullah Al Zakir Hossain <aazhbd@yahoo.com>
2020-03-30 22:17:54 +0200

    Updated home template and views.

--- qquote/qquote/urls.py
+++ qquote/qquote/urls.py
@@ -3,8 +3,11 @@ from django.urls import path, include
 
 from django.views.generic import TemplateView
 
+from quran import views
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html')),
+    path('', views.HomeView.as_view(), name='index'),
+    # path('', TemplateView.as_view(template_name='main.html')),
     path('admin/', admin.site.urls),
     path('baton/', include('baton.urls')),
     path('accounts/', include('allauth.urls')),
--- qquote/quran/views.py
+++ qquote/quran/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from quran.models import Chapter
+
+from django.views import generic
+
+class HomeView(generic.ListView):
+    model = Chapter
+    context_object_name = 'chapters'
+    template_name = 'home.html'
+
