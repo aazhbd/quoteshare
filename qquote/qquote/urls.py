219ba4732ebfac71693cec10c84babce8dbd71e7
Abdullah Al Zakir Hossain <aazhbd@yahoo.com>
2020-04-01 16:48:46 +0200

    Added page for for info.

--- qquote/qquote/urls.py
+++ qquote/qquote/urls.py
@@ -7,6 +7,7 @@ from quran import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
+    path('info', TemplateView.as_view(template_name='info.html'), name='info'),
     path('<int:chapter>/', views.ChapterView.as_view(), name='chapter'),
     path('<int:chapter>/<int:verse>/', views.VerseView.as_view(), name='verse'),
     path('admin/', admin.site.urls),
