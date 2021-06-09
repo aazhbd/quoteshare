a0d8f89a493da4f801d725ca2a1d3673ac6e935a
AAZ H <aazhbd@yahoo.com>
2021-06-09 12:44:02 +0200

    Blog models and pages updated.

--- content/home/models.py
+++ content/home/models.py
@@ -2,12 +2,34 @@ from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
 
 
 class HomePage(Page):
-    body = RichTextField(blank=True)
+    top_title = models.CharField(max_length=250, blank=True)
+    main_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    body = StreamField([
+        ('rich_text', blocks.RichTextBlock(icon='doc-full', label='Rich Text')),
+        ('html', blocks.RawHTMLBlock(icon='site', label='HTML'))
+    ])
+
+    search_fields = Page.search_fields + [
+        index.SearchField('top_title'),
+        index.SearchField('body')
+    ]
 
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full"),
+        ImageChooserPanel('main_image'),
+        FieldPanel('top_title'),
+        StreamFieldPanel('body')
     ]
