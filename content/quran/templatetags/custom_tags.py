0146b7140c2c647e7a1c8da0d98b692b1ef10357
AAZ H <aazhbd@yahoo.com>
2021-06-10 07:20:03 +0200

    Unicode updates for menu.

--- content/quran/templatetags/custom_tags.py
+++ content/quran/templatetags/custom_tags.py
@@ -8,3 +8,13 @@ def url_replace(request, field, value):
     dict_ = request.GET.copy()
     dict_[field] = value
     return dict_.urlencode()
+
+
+@register.filter
+def simplify(value):
+    import unicodedata
+    return unicodedata.normalize('NFC', value)\
+        .encode('ascii', 'ignore')\
+        .decode("utf-8")\
+        .replace('\'', '').replace('`', '')
+
