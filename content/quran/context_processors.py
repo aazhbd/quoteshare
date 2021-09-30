de68b23c0bd05087bca330443a2030d4fd2e8662
aazhbd <aazhbd@yahoo.com>
2021-09-30 17:49:13 +0200

    Some query fixes.

--- content/quran/context_processors.py
+++ content/quran/context_processors.py
@@ -1,9 +1,8 @@
 from quran.models import *
 
+
 def chapter_processor(request):
-    data = {}
-    data['chapters'] = Chapter.objects.all()
-    return data
+    return {'chapters': Chapter.objects.all()}
 
 
 def menu_processor(request):
@@ -12,5 +11,3 @@ def menu_processor(request):
     data['author_info'] = Author.objects.filter(id__in=[dauthor.author.id for dauthor in distincts]).order_by('name')
     data['language_info'] = Language.objects.filter(id__in=[dauthor.author.alang.id for dauthor in distincts]).order_by('name')
     return data
-
-
