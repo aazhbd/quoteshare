628c612134648e1a02b9ab78fd69d91da8e9787b
AAZ H <aazhbd@yahoo.com>
2021-09-30 17:01:44 +0200

    Some query fixes.

--- content/quran/context_processors.py
+++ content/quran/context_processors.py
@@ -8,9 +8,9 @@ def chapter_processor(request):
 
 def menu_processor(request):
     data = {}
-    dista = Verse.objects.raw('SELECT id, author_id FROM quran_verse GROUP BY author_id')
-    distinct_authors = [dauthor.author.id for dauthor in dista]
-    distinct_langauges = [dauthor.author.alang.id for dauthor in dista]
+    distincts = Verse.objects.values('author').distinct()
+    distinct_authors = [dauthor.author.id for dauthor in distincts]
+    distinct_langauges = [dauthor.author.alang.id for dauthor in distincts]
 
     data['author_info'] = Author.objects.filter(id__in=distinct_authors).order_by('name')
     data['language_info'] = Language.objects.filter(id__in=distinct_langauges).order_by('name')
