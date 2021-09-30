f2c5075423c2623e3b76680ee06c553659e1d074
aazhbd <aazhbd@yahoo.com>
2021-09-30 18:27:48 +0200

    Some query fixes.

--- content/quran/context_processors.py
+++ content/quran/context_processors.py
@@ -8,6 +8,6 @@ def chapter_processor(request):
 def menu_processor(request):
     data = {}
     distincts = Verse.objects.values('author').distinct()
-    data['author_info'] = Author.objects.filter(id__in=[dauthor['author'] for dauthor in distincts]).order_by('name')
-    data['language_info'] = Language.objects.filter(id__in=[dauthor.alang.id for dauthor in data['author_info']]).order_by('name')
+    data['author_info'] = Author.objects.filter(id__in=list(distincts.values_list('author', flat=True))).order_by('name')
+    data['language_info'] = Language.objects.filter(id__in=list(data['author_info'].values_list('alang', flat=True))).order_by('name')
     return data
