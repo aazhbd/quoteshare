b8f7dba897f0639c5b19f14001bfa3fa46e763f7
Abdullah Al Zakir Hossain <aazhbd@conveylive.com>
2022-01-07 07:11:51 +0100

    Main application and layouts.

--- content/content/settings/production.py
+++ content/content/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = True
 
-SECRET_KEY = 'django-insecure-psl!00xzg02o3_wlp!^5vq2b4_g-1y987qdzh(-l$+7#r(y&$a'
+SECRET_KEY = 'rhbrzudcp7q=o5$@55r&j1c1-f+z5h$z6s60h@^ulxms(=6a5a'
 
 ALLOWED_HOSTS = ['quran-contents.com', 'www.quran-contents.com']
 
