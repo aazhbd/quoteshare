46079f1b596cd0be83489b9e7e73e51220a66627
Abdullah Al Zakir Hossain <aazhbd@conveylive.com>
2022-01-07 07:16:39 +0100

    Main application and layouts.

--- content/content/wsgi.py
+++ content/content/wsgi.py
@@ -1,8 +1,14 @@
-
 import os
+
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "content.settings.production")
 
+START = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 application = get_wsgi_application()
+application = WhiteNoise(application, root=START + '/static')
+application.add_files(START + '/media', prefix='media/')
+
