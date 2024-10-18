102fdbb6a773ed85316a15d5943e701c7b382984
Abdullah Al Zakir Hossain <aazhbd@yahoo.com>
2024-10-18 20:25:19 +0200

    Code updated.

--- contents/quran/wagtail_hooks.py
+++ contents/quran/wagtail_hooks.py
@@ -1,4 +1,6 @@
-from wagtail.contrib.modeladmin.options import (ModelAdmin, modeladmin_register)
+#from wagtail.contrib.modeladmin.options import (ModelAdmin, modeladmin_register)
+
+from wagtail_modeladmin.options import (ModelAdmin, modeladmin_register)
 
 from .models import *
 
